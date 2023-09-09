439ce4b6b3330081cf931f17c57bceed91f77687
Aranyak Das <das.aranyak17@gmail.com>
2023-09-09 21:27:00 +0530

    Till Carousel

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Navbar from "./Navbar";
+import Banner from "./Banner";
+import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+
+const Component = styled(Box)`
+  padding: 20px 10px;
+  background-color: #f2f2f2;
+`;
 
 function Home() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />;
+      <Component>
+        <Banner />
+      </Component>
+    </>
+  );
 }
 
 export default Home;
